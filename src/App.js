426b06dc4fb01c319dd3818d938067741e6c1d45
Mahendrakar Saivenkat <mahendrakar.saivenkat@publicissapient.com>
2022-09-27 23:11:17 +0530

    remove react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import Dashboard from "./features/dashboard/dashboard.component";
-import Details from "./features/details/details.component";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Dashboard />}></Route>
-      <Route path="/abc" element={<Details />}></Route>
-    </Routes>
-  );
+  return <Dashboard />;
 };
 
 export default App;
